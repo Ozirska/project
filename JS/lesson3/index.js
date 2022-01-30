8bc87e11178943ad042847425a4a7477bde95f27
Ozirska <m.ozirska2000@gmail.com>
2022-01-30 22:47:56 +0200

    new

---
+++ JS/lesson3/index.js
@@ -0,0 +1,34 @@
+// let n = 159;
+// let sum = 0;
+// while (n > 0) {
+//   sum += n;
+//   n -= 1;
+// }
+
+// console.log("Result:" + sum);
+
+// const n = 159;
+// let sum = 0;
+// for (let n = 159; n > 0; n--) {
+//   sum += n;
+// }
+// console.log("Result:" + sum);
+
+// const m = 7;
+// const n = 11;
+// let result = 1;
+
+// for (let m = 7; m <= 11; m++) {
+//   if (m % 2 === 1) {
+//     result *= m;
+//   }
+// }
+
+// console.log(result);
+
+for (let i = 1; i <= 5; i++) {
+  console.log("\n=> " + i + " <=\n\n");
+  for (let j = 1; j <= 10; j++) {
+    console.log(i + " x " + j + " = " + i * j);
+  }
+}
---
+++ part1/README.md
@@ -0,0 +1,13 @@
+Site link: file:///C:/Users/marin/Desktop/project/part1/index.html#
+
+Layout supports mobile (up-to-400) and desktop (up-to-1400) versions
+
+The tech stack is:
+HTML5
+CSS3
+Flexbox
+Sass (Scss)
+BEM methodology
+
+Author:
+Marina Ozirska
